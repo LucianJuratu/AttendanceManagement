f4403b9b34d673edda1a2bd09ee7547b82353025
LucianJuratu <74536036+LucianJuratu@users.noreply.github.com>
2022-01-10 13:37:52 +0200

    Delete stage 3 directory

--- stage 3/application.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(
-    __name__,
-    # static_path="./"
-    # static_url_path="/static/",
-    # static_folder="static",
-)
-app.secret_key = 'supersecretkey'
-import views  # NOQA
