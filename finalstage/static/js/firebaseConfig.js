50613aad13e572dde787b1e102d820ba4717c64b
LucianJuratu <74536036+LucianJuratu@users.noreply.github.com>
2022-01-10 13:38:52 +0200

    Add files via upload

---
+++ finalstage/static/js/firebaseConfig.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC2XljF65Ac6ybNhoQAmdwD1E_o11xItGw",
+    authDomain: "attendance-management-2296e.firebaseapp.com",
+    projectId: "attendance-management-2296e",
+    storageBucket: "attendance-management-2296e.appspot.com",
+    messagingSenderId: "621691891391",
+    appId: "1:621691891391:web:50107625d23f2b6a94a00a"
+    databaseURL:"https://attendance-management-2296e-default-rtdb.europe-west1.firebasedatabase.app/"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
